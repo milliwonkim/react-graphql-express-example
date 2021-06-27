dc9ad3336c5c7fbab16c9f0f0782c407ddd98e25
milliwonkim <rldnjs9347@gmail.com>
2021-06-27 22:20:32 +0900

    [feat] add auth with graphql

---
+++ client/components/AuthForm.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+export class AuthForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { email: '', password: '' };
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state);
+    }
+
+    render() {
+        return (
+            <div className='row'>
+                <form onSubmit={(e) => this.onSubmit(e)} className='col s6'>
+                    <div className='input-field'>
+                        <input
+                            placeholder='email'
+                            value={this.state.email}
+                            onChange={(e) =>
+                                this.setState({ email: e.target.value })
+                            }
+                        />
+                    </div>
+                    <div className='input-field'>
+                        <input
+                            placeholder='password'
+                            value={this.state.password}
+                            onChange={(e) =>
+                                this.setState({ password: e.target.value })
+                            }
+                        />
+                    </div>
+                    <div className='errors'>
+                        {this.props.errors.map((error) => (
+                            <div key={error}>{error}</div>
+                        ))}
+                    </div>
+                    <button className='btn'>Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default AuthForm;
